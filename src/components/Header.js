bc11d8c0593594e6f4b4788c09baa912e0c5b478
Lauren Wollaston <laurenwollaston@outlook.com>
2024-01-18 19:53:53 -0600

    changed navbar glow

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import '../layouts/header.css';
 const Header = ({ page, setPage }) => {
   return (
     <div className="header">
-      <h1 style={{marginRight:"auto"}}>Lauren Wollaston</h1><p style={{marginLeft:"auto"}}><Navbar setPage={setPage} /></p>
+      <h1 style={{marginRight:"auto"}}>Lauren Wollaston</h1><p style={{marginLeft:"auto"}}><Navbar page={page} setPage={setPage} /></p>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../layouts/header.css'
 
-const Navbar = ({ setPage }) => {
+
+const Navbar = ({page, setPage }) => {
     return (
       <nav>
         <ul>
-          <button onClick={() => setPage('About Me')}><li>About Me</li></button>
-          <button onClick={() => setPage('Portfolio')}><li>Portfolio</li></button>
-          <button onClick={() => setPage('Resume')}><li>Resume</li></button>
+          <button id={page==='About Me'? 'activated':''} onClick={() => setPage('About Me')}><li>About Me</li></button>
+          <button id={page==='Portfolio'? 'activated':''} onClick={() => setPage('Portfolio')}><li>Portfolio</li></button>
+          <button id={page==='Resume'? 'activated':''} onClick={() => setPage('Resume')}><li>Resume</li></button>
         </ul>
       </nav>
     );
